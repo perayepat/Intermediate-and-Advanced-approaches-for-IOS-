63f2882e2384f7d40403534579362e4fa236347b
Mphathi Maapola <perrierpat@icloud.com>
2023-02-19 09:39:40 +0200

    Using filter and map

--- Collections.playground/Pages/Lazy Iteration.xcplaygroundpage/Contents.swift
+++ Collections.playground/Pages/Lazy Iteration.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
+let indexes = 1..<5000
+
+let images = indexes.lazy.filter{ index -> Bool in
+    print("[Filter]")
+    return index % 2 == 0
+}.map { index -> String in
+    print("[Map]")
+    return "image_\(index)"
+}
+
+var chosenImages = images.suffix(3)
+for image in chosenImages {
+    print(image)
+}
+
+print(images.suffix(3))
 
 
 //: [Next](@next)
