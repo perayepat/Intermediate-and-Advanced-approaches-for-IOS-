e16d6f119f7e613c3282140820857f7ef2390c07
Mphathi Maapola <perrierpat@icloud.com>
2023-02-19 09:52:05 +0200

    Reduce into implementation

---
+++ Collections.playground/Pages/Reduce Into.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let ratings = [4, 8.5, 9.5, 2, 6, 3, 5.5, 7, 2.8, 9.8, 5.9, 1.5]
+/// We want to take these ratings and convert them into a dictionary where we can see how many bad and good reatings we have based on the ratings array using the reduce function
+
+
