65107539084b51ce1dcb21271b1fa063dd44fe47
Mphathi Maapola <perrierpat@icloud.com>
2023-02-19 10:15:16 +0200

    Zip implantation Done

--- Collections.playground/Pages/Zip.xcplaygroundpage/Contents.swift
+++ Collections.playground/Pages/Zip.xcplaygroundpage/Contents.swift
@@ -1,11 +1,16 @@
 //: [Previous](@previous)
 
 import Foundation
-
+/// The zip fucntion can take two different sequences and paiur them together but if the arrays are no the same length it will take the smaller array
 let students = ["Alex", "Mary", "John", "Steven"]
-let grades = [3.4, 2.8,3.8, 4]
+let grades = [3.4, 2.8, 4.0, 3.0]
 
 ///We want to iterate through both of these arrays and create a pairing
+let pair = zip(students, grades)
 
+for studentsAndGrade in pair {
+    print(studentsAndGrade.0)
+    print(studentsAndGrade.1)
+}
 
 //: [Next](@next)
