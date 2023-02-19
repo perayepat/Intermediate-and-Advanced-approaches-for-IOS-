c3b14c0a8140e6682e15da925ad7027aad967627
Mphathi Maapola <perrierpat@icloud.com>
2023-02-19 10:12:04 +0200

    Zip implementation started

---
+++ Collections.playground/Pages/Zip.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let students = ["Alex", "Mary", "John", "Steven"]
+let grades = [3.4, 2.8,3.8, 4]
+
+///We want to iterate through both of these arrays and create a pairing
+
+
+//: [Next](@next)
