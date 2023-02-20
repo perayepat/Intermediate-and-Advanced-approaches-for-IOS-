d911d63564949cdb070bc1bcbb65cd7217211aa9
Mphathi Maapola <perrierpat@icloud.com>
2023-02-20 19:32:20 +0200

    New way using enums is being used

--- Enumerations .playground/Pages/AirplaneTicket.xcplaygroundpage/Contents.swift
+++ Enumerations .playground/Pages/AirplaneTicket.xcplaygroundpage/Contents.swift
@@ -62,19 +62,45 @@ struct Economy{
 struct FirstClass{
     let departure: String
     let arrival: String
-    let meal: String
+    let meal: Bool
 }
 
 struct Business{
     let departure: String
     let arrival: String
-    let meal: String
+    let meal: Bool
     let chargingPorts: Bool
+}
+
+struct International {
+    let departure: String
+    let arrival: String
+    let meal: Bool
+    let chargingPorts: Bool
+    let baggageAllowed: Bool
 }
 
 enum Ticket{
     case economy(Economy)
     case firstClass(FirstClass)
     case business(Business)
+    case international(International)
 }
 
+let ticket = Ticket.business(Business(departure: "Houston", arrival: "Denver", meal: true, chargingPorts: true))
+
+/// If you don't need to handle all the tickets use if case let
+func checkIn(ticket: Ticket){
+    switch ticket{
+    case .economy(let economy):
+        print(economy)
+    case .firstClass(let firstClass):
+        print(firstClass)
+    case .business(let business):
+        print(business)
+    case .international(let international):
+        print(international)
+    }
+}
+
+checkIn(ticket: ticket)
