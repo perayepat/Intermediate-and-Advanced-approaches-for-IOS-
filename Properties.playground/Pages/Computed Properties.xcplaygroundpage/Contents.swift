ca0189ad24390446ab577a7238c5df6060605003
Mphathi Maapola <perrierpat@icloud.com>
2023-02-21 22:07:50 +0200

    Example of a computed property to get the total of the items in a class

--- Properties.playground/Pages/Computed Properties.xcplaygroundpage/Contents.swift
+++ Properties.playground/Pages/Computed Properties.xcplaygroundpage/Contents.swift
@@ -16,4 +16,28 @@ let end = Date()
 let workout = Workout(startTime: start, endTime: end)
 //print(workout.endTime.timeIntervalSince(workout.startTime))
 //Using a computed variable
-print(workout.timeElapsed)
+//print(workout.timeElapsed)
+
+//MARK: - Example 2
+/// how do we calculate the total of our cart , using a computed property
+struct CartItem {
+    let name: String
+    let price: Double
+}
+
+struct Cart{
+    let items: [CartItem]
+    var total: Double {
+        items.reduce(0){
+            // this allows us to get a running total
+            return $0 + $1.price
+        }
+    }
+}
+
+let items = [CartItem(name: "Bread" ,price: 4.50),CartItem(name: "Mild" ,price: 5.50), CartItem(name: "Cheese(Mortadelle)" ,price: 6.50)]
+
+let cart = Cart(items: items)
+print(cart.total)
+
+
