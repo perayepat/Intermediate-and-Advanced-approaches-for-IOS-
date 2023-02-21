61e4fb730e4ea26b74b92c545a1adee6b50caa30
Mphathi Maapola <perrierpat@icloud.com>
2023-02-21 21:49:30 +0200

    Lazy properties implemented

--- Properties.playground/Pages/Lazy Stored Properties.xcplaygroundpage/Contents.swift
+++ Properties.playground/Pages/Lazy Stored Properties.xcplaygroundpage/Contents.swift
@@ -1,6 +1,9 @@
 import Foundation
 //MARK: - Scenario
 /// Building an app for a school system with exams
+/// Because we are using a lazy property the wait isnt presernt
+///
+/// Lazy properties are only initialized when you are trying to access them not when they're initialized 
 enum Level{
     case easy
     case medium
@@ -10,9 +13,9 @@ enum Level{
 struct Exam{
     var level: Level
     
-    lazy var questions :[String] = {
+    lazy private(set) var questions :[String] = {
         //simulate time to get from API
-        sleep(2)
+        sleep(4)
         switch level{
         case .easy:
             return ["What is 1+2","What is a question"]
@@ -24,6 +27,15 @@ struct Exam{
     }()
 }
 
+// Because it's a lazy property once it's intialized it cant be changed
+
 var exam = Exam(level: .easy)
+//print(exam.questions)
+// Creating a copy of the exam
+var hardExam = exam
+hardExam.level = .hard
+print(hardExam.questions)
+print("Wait for a second love")
+sleep(2)
 print(exam.questions)
 
