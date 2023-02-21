007ae029217796e573490de23e717851b3d32b88
Mphathi Maapola <perrierpat@icloud.com>
2023-02-21 21:36:50 +0200

    Basic implementation added of a computed property

--- Properties.playground/Pages/Lazy Stored Properties.xcplaygroundpage/Contents.swift
+++ Properties.playground/Pages/Lazy Stored Properties.xcplaygroundpage/Contents.swift
@@ -1,7 +1,29 @@
-//: [Previous](@previous)
-
 import Foundation
+//MARK: - Scenario
+/// Building an app for a school system with exams
+enum Level{
+    case easy
+    case medium
+    case hard
+}
 
-var greeting = "Hello, playground"
+struct Exam{
+    var level: Level
+    
+    var questions: [String]{
+        //simulate time to get from API
+        sleep(5)
+        switch level{
+        case .easy:
+            return ["What is 1+2","What is a question"]
+        case .medium:
+            return ["What is 1 X 2","Why does it rain"]
+        case .hard:
+            return ["What is 8 / 2 ", "When does conciousness form"]
+        }
+    }
+}
 
-//: [Next](@next)
+let exam = Exam(level: .easy)
+print(exam.questions)
+
