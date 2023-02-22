c3c241614ef9faa5d62724a61fce023ad7687a07
Mphathi Maapola <perrierpat@icloud.com>
2023-02-22 07:41:00 +0200

    Unwrapping optional techniques

---
+++ Optionals/Optionals/Unwrapping Techniques.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Student{
+    let firstName: String
+    let lastName: String
+    var middleName: String?
+
+}
---
+++ Optionals/Optionals/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+var estudiante = Student(firstName: "Jeoung", lastName: "Gou")
+estudiante.middleName = "Joungshon"
+
+print(estudiante.middleName)
+
+
