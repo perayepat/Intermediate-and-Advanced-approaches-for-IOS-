c895e45d2979ead848880b069ad48017f1cba02f
Mphathi Maapola <perrierpat@icloud.com>
2023-02-22 08:04:14 +0200

    Using a switch for optional names

--- Optionals/Optionals/Return Optional Strings.swift
+++ Optionals/Optionals/Return Optional Strings.swift
@@ -1,8 +1,22 @@
-//
-//  Return Optional Strings.swift
-//  Optionals
-//
-//  Created by Pat on 2023/02/22.
-//
-
 import Foundation
+
+struct UniStudent{
+    let firstName: String?
+    let lastName: String?
+    
+    var displayName: String? {
+        switch (firstName, lastName){
+        case let (first?, last?): return "\(first) \(last)"
+        case let (first?, nil): return first
+        case let (nil, last?): return last
+        default: return nil
+        }
+    }
+    
+    func createGreetingMessage() -> String{
+        let message = """
+            Dear \(displayName), welcome to swift
+        """
+        return message
+    }
+}
