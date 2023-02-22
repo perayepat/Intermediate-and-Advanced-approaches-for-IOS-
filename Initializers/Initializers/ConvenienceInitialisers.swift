b5211901dd40d520f5367e716f416911f4f8247c
Mphathi Maapola <perrierpat@icloud.com>
2023-02-22 07:34:31 +0200

    Using final class and required initialisers

--- Initializers/Initializers/ConvenienceInitialisers.swift
+++ Initializers/Initializers/ConvenienceInitialisers.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-class Car {
+protocol CarType{
+    init(make: String, model: String)
+}
+/// Adding Final meas you cannot subcalss and marks you classes so they can't be subclassed ,
+/// When you don't want to inherit from the car class you use the final 
+
+class Car : CarType{
     var make: String
     var model: String
     var color: String
@@ -12,9 +18,15 @@ class Car {
     }
     
     //Sets some defaults and allows the caller to set arguments when they want
-    convenience init(make: String , model: String){
+    /// Another way they are useful is using them with the init
+    required convenience init(make: String , model: String){
         self.init(make: make, model: model, color: "White")
     }
-    
-    
+    /*
+    class func makeCart(make: String, model: String) -> Self {
+        let car = self.init(make: make, model: model)
+        
+        return car
+    }
+     */
 }
