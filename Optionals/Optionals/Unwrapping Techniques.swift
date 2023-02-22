0a0e3256ebfb67a0b878126a4eebed252f869508
Mphathi Maapola <perrierpat@icloud.com>
2023-02-22 07:53:34 +0200

    Using variable shadowing

--- Optionals/Optionals/Unwrapping Techniques.swift
+++ Optionals/Optionals/Unwrapping Techniques.swift
@@ -1,9 +1,44 @@
 import Foundation
 
-struct Student{
+struct Student: CustomStringConvertible{
     let firstName: String
     let lastName: String
     var middleName: String?
     var grade: String?
-
+    
+    var description: String{
+        var studentDescription = "\(firstName),"
+        if let middleName = middleName{
+            //This is variable shadowing
+            studentDescription += "\(middleName)"
+        }
+        
+        if let grade = grade{
+            studentDescription.append(" \(grade)")
+        }
+        return studentDescription
+    }
 }
+
+
+//1, if let
+/// Can be used to unwrap multiple optionals
+/// only unrwrapped when both are unwrapped successfuly
+/*
+if let middleName = estudiante.middleName ,
+   let grade = estudiante.grade{
+    print(middleName)
+    print(grade)
+}
+*/
+
+//2
+/*
+func displayStudent(student: Student){
+    
+    guard let middleName = estudiante.middleName,
+          let grade = estudiante.grade else { return}
+    print(middleName,grade)
+}
+ */
+
--- Optionals/Optionals/main.swift
+++ Optionals/Optionals/main.swift
@@ -5,17 +5,7 @@ var estudiante = Student(firstName: "Jeoung", lastName: "Gou")
 estudiante.middleName = "Joungshon"
 estudiante.grade = "A"
 
-//1, if let
-/// Can be used to unwrap multiple optionals
-/// only unrwrapped when both are unwrapped successfuly
-/*
-if let middleName = estudiante.middleName ,
-   let grade = estudiante.grade{
-    print(middleName)
-    print(grade)
-}
-*/
+print(estudiante)
 
-print(estudiante.middleName)
 
 
